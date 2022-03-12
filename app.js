1ae91b1a95d47668472342075bd661296a61fc1d
Jiahui Huang <jiahuihuang@Jiahuis-MacBook-Pro.local>
2022-03-12 12:59:02 -0500

    install styling

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-const routes = require("./route");
 
+app.use(express.static("public"));
 app.get('/components', function(req,res){
-    res.render('CONTENT');
+    res.render('content');
 })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use("/", routes);
 app.listen(3000);
 
 module.exports = app;
--- route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", function (req, res) {
-  res.render("CONTENT", { title: "Food blog page" });
-});
-
-module.exports = router;
